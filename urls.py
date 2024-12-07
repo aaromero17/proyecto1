2653ad650d3a5642ee0788bb3800f596baee5808
aaromero <aaromero17@gmail.com>
2024-12-07 17:04:29 -0400

    se modificaron wiews y urls

--- urls.py
+++ urls.py
@@ -16,10 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from proyecto1.views import saludo
+from proyecto1.views import saludo,damefecha,calculaedad
+
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('saludo/', saludo),
+    path('fecha/', damefecha),
+    #ejemplo    http://127.0.0.1:8000/edad/edad/2034
+    path('edad/<int:edad>/<int:year>',calculaedad ),
 ]
--- views.py
+++ views.py
@@ -1,5 +1,32 @@
+import datetime
 from django.http import HttpResponse
 
 def saludo(request):#primera vista
 
     return HttpResponse("Hola mundo")
+
+def damefecha(request):
+    fechaactual=datetime.datetime.now()
+
+    documento=f"""
+        <html>
+        <body>
+        <h1>
+        fecha y hora actuales: {fechaactual}
+        </h1>
+        </body>
+        
+        </html>
+        
+        """
+
+
+    return HttpResponse(documento)
+
+def calculaedad(request,edad,year):
+    
+    edadpara=year-2024
+    edad=edad+edadpara
+    documento=f"La edad para el año {year} sera de {edad}"
+
+    return HttpResponse(documento)
